13c1c38642473d7bbc93069ad4f4aed2c9c42196
Rice Rodriguez <rice.rodriguez@ttu.edu>
2019-07-21 10:52:56 -0500

    Outputs are latched and work correctly. Now to actually decode to FS

--- src/instr_dec.v
+++ src/instr_dec.v
@@ -5,7 +5,7 @@
  determines how many instructions to expect, then counts until that
  number of instructions has passed.
  TODO:
-   - LATCH ALL THE OUTPUTS
+   x LATCH ALL THE OUTPUTS
    - DECODE OP CODES TO FS CODES
 */
 `include "msp430_ops.vh"
@@ -14,6 +14,9 @@ module instr_dec
    input [15:0]      MDB_out,
    input             CTL_SEL,
    output reg [1:0]  FORMAT,
+   output reg [5:0]  FS,
+   output reg        BW,
+   output reg        RW,
    output reg [3:0]  reg_SA, reg_DA,
    output reg [2:0]  AdAs, 
    output reg [15:0] reg_Din,
@@ -27,13 +30,14 @@ module instr_dec
    localparam FMT_I = 1, FMT_II = 2, FMT_J = 3;
 
    // Registers
-   reg              start;   // Start counting instructions
-   // reg              advance; // Instruction has finished
    reg [2:0]        count;   // Counts instructions
 
    // Initialize registers
    initial
      begin
+        RW <= 0;
+        BW <= 0;
+        FS <= 0;
         FORMAT <= 0;
         reg_SA <= 0;
         reg_DA <= 0; 
@@ -43,7 +47,6 @@ module instr_dec
         MSP <= 0;
         MSR <= 0;        
         count <= 0;
-        start <= 0;
         // advance <= 0;
      end
 
@@ -53,7 +56,7 @@ module instr_dec
    
 
    // Assign wires
-   assign INSTRUCTION = (start) ? (16'bx) : MDB_out;
+   assign INSTRUCTION = (!count) ? MDB_out : 16'bx;
    // FORMAT: 1 = Format 1, 2 = Format 2, 3 = Jump, X = Unknown
    assign FORMAT_ASYNC = (INSTRUCTION[15:13] == `OP_JUMP) ? FMT_J  :
                          (INSTRUCTION[15:12] == 4'b0001)  ? FMT_II :
@@ -76,32 +79,48 @@ module instr_dec
         AdAs[1:0] <= (FORMAT_ASYNC <= FMT_II) ? INSTRUCTION[5:4] : 2'bx;
      end  
 
+
    // Determines instruction length
    always @ (negedge clk)
      begin
-        if (~start)
+        if (!count)
           begin
+             // Tell PC to go to next PC on the next clock tick
              MPC <= 1;
-             MSR <= 1;          
-             reg_Din <= reg_Din;             
+             // Tell SR to use status bits from function unit
+             MSR <= 1;
+             // Latch Din, MUX Din will take care of which one to pick
+             reg_Din <= reg_Din;
+             // Is this a jump?
              if (FORMAT == FMT_J)
+               // If so, set PC to shift the offset
                MPC <= 3;
-             else if (FORMAT == FMT_I || FORMAT == FMT_II)
-               begin
-                  if (INSTRUCTION[5] || !INSTRUCTION[5:4])
-                    begin
-                       count <= 1;
-                       start <= 1;
-                       if ((FORMAT == FMT_I) && ~INSTRUCTION[7])
-                         count <= count + 1;                       
-                    end
-               end
+             // Otherwise, is it one of the other two valid formats?
+             else if (FORMAT_ASYNC == FMT_I || FORMAT_ASYNC == FMT_II)
+                casex (INSTRUCTION[5:4])
+                  2'bx0:
+                    // If As = 00 or 10, then the next word should be an instruction,
+                    // unless the destination addressing mode is indexed
+                    if ((FORMAT_ASYNC == FMT_I) && INSTRUCTION[7])
+                      count <= 1;
+                  2'b11:
+                    // If As = 11, then it's either immediate or indirect
+                    // autoincrement. The only difference is the source
+                    // register. So check if there is anything there.
+                    if ((FORMAT_ASYNC == FMT_I) && !INSTRUCTION[11:8])
+                      count <= 1;
+                  2'b01:
+                    if ((FORMAT_ASYNC == FMT_I) && INSTRUCTION[7])
+                      count <= 2;
+                    else
+                      count <= 1;
+                  default: count <= 0;
+                endcase // casex (INSTRUCTION[5:4])
           end // if (~start)
         else
           begin
              // Set PC and SR to hold value
              {MPC,MSR} <= 0;
-             // reg_Din <= (INSTRUCTION === 16'bx) ? MDB_out : reg_Din;
              // Decrement counter of instruction length
              if (count)
                begin
@@ -110,9 +129,6 @@ module instr_dec
                   // actually an instruction
                   reg_Din <= MDB_out;                  
                end
-             // If we hit 0 then this is the last non-instruction
-             else
-               start <= 0;
           end  
      end // always @ (negedge clk)
 
--- src/sim/instr_dec_tb.v
+++ src/sim/instr_dec_tb.v
@@ -3,10 +3,13 @@ module instr_dec_tb;
    /*AUTOWIRE*/
    // Beginning of automatic wires (for undeclared instantiated-module outputs)
    wire [2:0]           AdAs;                   // From uut of instr_dec.v
+   wire                 BW;                     // From uut of instr_dec.v
    wire [1:0]           FORMAT;                 // From uut of instr_dec.v
+   wire [5:0]           FS;                     // From uut of instr_dec.v
    wire [2:0]           MPC;                    // From uut of instr_dec.v
    wire [1:0]           MSP;                    // From uut of instr_dec.v
    wire                 MSR;                    // From uut of instr_dec.v
+   wire                 RW;                     // From uut of instr_dec.v
    wire [3:0]           reg_DA;                 // From uut of instr_dec.v
    wire [15:0]          reg_Din;                // From uut of instr_dec.v
    wire [3:0]           reg_SA;                 // From uut of instr_dec.v
@@ -22,10 +25,13 @@ module instr_dec_tb;
      (/*AUTOINST*/
       // Outputs
       .AdAs                             (AdAs[2:0]),
+      .BW                               (BW),
       .FORMAT                           (FORMAT[1:0]),
+      .FS                               (FS[5:0]),
       .MPC                              (MPC[2:0]),
       .MSP                              (MSP[1:0]),
       .MSR                              (MSR),
+      .RW                               (RW),
       .reg_DA                           (reg_DA[3:0]),
       .reg_Din                          (reg_Din[15:0]),
       .reg_SA                           (reg_SA[3:0]),
